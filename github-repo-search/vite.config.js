66aa5c195a2dd4d5b00b8adc7be4c5cd3efa16d6
Anastasia <dzheyn.anna@mail.ru>
2024-08-09 23:55:28 +0400

    fix: добавляет base

--- github-repo-search/vite.config.js
+++ github-repo-search/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
-    base:'/github-repo-search/',
+    base: '/github-repo-search/',
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),

2019899f8cf4002eb4bb39f3c19ff4df1dcaa4f5
Anastasia <dzheyn.anna@mail.ru>
2024-08-12 01:46:17 +0400

    fix: правит формат файла

--- github-repo-search/src/store/repository-data/api-actions.ts
+++ github-repo-search/src/store/repository-data/api-actions.ts
@@ -1,12 +1,11 @@
-// import { adaptRepositoriesToClient } from "@/utils/adapterToClient";
 import { REDUCER_NAME } from "@/utils/constant";
 import { RepositoriesData, Repository } from "@frontend-types/repository.interface";
 import { AppDispatch, State } from '@frontend-types/state.type';
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import { adaptRepositoriesToClient } from "@utils/adapterToClient";
 import { ActionName } from '@utils/constant';
 import { GraphQLClient } from "graphql-request";
-import SearchRepositories  from "./search-query.graphql";
-import { adaptRepositoriesToClient } from "@/utils/adapterToClient";
+import SearchRepositories from "./search-query.graphql";
 
 // Асинхронное действие для получения списка репозиториев по искомой строке
 export const fetchRepositories = createAsyncThunk<
